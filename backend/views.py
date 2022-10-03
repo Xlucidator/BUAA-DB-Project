d5b630fe191659caa7607f173fb6911237926a17
rain1732 <3044550844@qq.com>
2022-10-03 11:06:23 +0800

    jwt

--- backend/views.py
+++ backend/views.py
@@ -1,8 +1,18 @@
+from datetime import datetime, timedelta
+
+import jwt
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.db import connection
 
 from rest_framework_jwt.settings import api_settings
+
+
+class User:
+    username = ''
+
+    def __init__(self, name):
+        self.username = name
 
 
 # Create your views here.
@@ -31,26 +41,38 @@ def check_user(username, password):
     return result
 
 
+def get_jwt(username, role_data='default'):
+    # jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER  # 生payload部分的方法
+    # payload = jwt_payload_handler(user)  # 生成payload, 得到字典
+    # encoded_jwt = jwt.encode(payload, JWT_SECRET_KEY, algorithm='HS256')
+    jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER  # 生成jwt的方法
+    payload = {
+        'exp': datetime.utcnow() + timedelta(seconds=3600),  # 单位秒
+        'iat': datetime.utcnow(),
+        'data': {'username': username, 'role_data': role_data}
+    }
+    token = jwt_encode_handler(payload)  # 生成jwt字符串
+
+    return str(token)
+
+
 def login(request):
-
-    # {'exp': xxx, 'email': '', 'user_id': 1, 'username': 'admin'}
-    # user：登录的用户对象
-
     username = request.POST.get("username")
     password = request.POST.get("password")
-    print(username, password)
+    # print(username, password)
     result = check_user(username, password)
     if result:
-
         date_msg = "success!"
         date_flag = "yes"
-
+        token = get_jwt(username)
+        date = {'flag': date_flag, 'msg': date_msg, 'token': token}
+        return JsonResponse({'request': date})
     else:
         date_msg = "the code is wrong"
         date_flag = "no"
+        date = {'flag': date_flag, 'msg': date_msg}
+        return JsonResponse({'request': date})
 
-    date = {'flag': date_flag, 'msg': date_msg}
-    return JsonResponse({'request': date})
 
 def add_person(username, password, permission):
     with connection.cursor() as cursor:
