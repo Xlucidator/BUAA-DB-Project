3e054265d5d1834f7c32ca34c0c61d7d89c7783c
rain1732 <3044550844@qq.com>
2022-10-06 21:52:41 +0800

    jwt密钥添加

--- backend/views.py
+++ backend/views.py
@@ -28,15 +28,16 @@ def check_user(username, password):
 def get_jwt(username, role_data='default'):
     # jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER  # 生payload部分的方法
     # payload = jwt_payload_handler(user)  # 生成payload, 得到字典
-    # encoded_jwt = jwt.encode(payload, JWT_SECRET_KEY, algorithm='HS256')
-    jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER  # 生成jwt的方法
+    #
+    # jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER  # 生成jwt的方法
     payload = {
         'exp': datetime.utcnow() + timedelta(seconds=3600),  # 单位秒
         'iat': datetime.utcnow(),
         'data': {'username': username, 'role_data': role_data}
     }
-    token = jwt_encode_handler(payload)  # 生成jwt字符串
-
+    JWT_SECRET_KEY = "123"
+    token = encoded_jwt = jwt.encode(payload, JWT_SECRET_KEY, algorithm='HS256')
+    # print(jwt.decode(token,JWT_SECRET_KEY))
     return str(token)
 
 
