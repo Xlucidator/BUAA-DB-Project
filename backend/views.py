a47d3ccc2c2e6fe469a63cc16678d6287095e2ae
rain1732 <3044550844@qq.com>
2022-10-15 21:36:16 +0800

    '修改了数据表的顺序对应问题，增加了一些返回说明'

--- backend/views.py
+++ backend/views.py
@@ -1,25 +1,13 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from backend.tools import get_jwt, check_user, judge, all_users, modify_user, delete_user, add_person, consent, reject, \
-    one_user, add_into_queue, all_applications, modify_application
+    one_user, add_into_queue, all_applications, modify_application, success, fail
 
-SUCCESS_DATA = {'flag': 'yes', 'msg': 'success!'}
-FAIL_DATA = {'flag': 'no', 'msg': 'fail'}
-NOT_ALLOWED_DATA = {'flag': 'no', 'msg': 'not allowed'}
-
-
-# 代码重复度太高了，应该去寻找一些方法进行替换
 
 def login(request):
     CodeName = request.POST.get("CodeName")
     Password = request.POST.get("Password")
-    result = check_user(CodeName, Password)
-    if result:
-        token = get_jwt(CodeName)
-        d = {'CodeName': CodeName, 'token': token}
-        return JsonResponse({'request': SUCCESS_DATA, 'result': d})
-    else:
-        return JsonResponse({'request': FAIL_DATA})
+    return check_user(CodeName, Password)
 
 
 def enroll(request):
@@ -29,18 +17,16 @@ def enroll(request):
     Race = request.POST.get("Race")
     Description = request.POST.get("Description")
     Password = request.POST.get("Password")
-    print("test1\n")
     return add_into_queue(CodeName, Class, Region, Race, Description, Password)
 
 
 def users_get(request):
     token = request.POST.get("token")
     allowance = judge(token, 1)
-    print(allowance)
     if allowance:
-        return JsonResponse({'request': SUCCESS_DATA, 'result': all_users()})
+        return success('成功', all_users())
     else:
-        return JsonResponse({'request': NOT_ALLOWED_DATA})
+        return fail('无访问权限')
 
 
 def user_get(request):
@@ -56,7 +42,7 @@ def user_modify(request):
     if allowance:
         return modify_user(code_name, permission)
     else:
-        return JsonResponse({'request': NOT_ALLOWED_DATA})
+        return fail('无访问权限')
 
 
 def user_delete(request):
@@ -66,7 +52,7 @@ def user_delete(request):
     if allowance:
         return delete_user(name)
     else:
-        return JsonResponse({'request': NOT_ALLOWED_DATA})
+        return fail('无访问权限')
 
 
 def application_reject(request):
@@ -76,7 +62,7 @@ def application_reject(request):
     if allowance:
         return reject(name)
     else:
-        return JsonResponse({'request': NOT_ALLOWED_DATA})
+        return fail('无访问权限')
 
 
 def application_consent(request):
@@ -87,16 +73,16 @@ def application_consent(request):
     if allowance:
         return consent(name, Permission)
     else:
-        return JsonResponse({'request': NOT_ALLOWED_DATA})
+        return fail('无访问权限')
 
 
 def applications_get(request):
     token = request.POST.get("token")
     allowance = judge(token, 1)
     if allowance:
-        return JsonResponse({'request': SUCCESS_DATA, 'result': all_applications()})
+        return success('成功获取', all_applications())
     else:
-        return JsonResponse({'request': NOT_ALLOWED_DATA})
+        return fail('无访问权限')
 
 
 # 后端调试使用
@@ -120,4 +106,4 @@ def application_modify(request):
     if allowance:
         return modify_application(CodeName, Permission, Class, Region, Race, Description)
     else:
-        return JsonResponse({'request': NOT_ALLOWED_DATA})
+        return JsonResponse({'request': fail('无访问权限')})
