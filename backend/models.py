b3870c93efd09f8f26a9510b46f0e4151ff08f3a
rain1732 <3044550844@qq.com>
2022-11-13 17:01:53 +0800

    替换为drf

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class UserAccount(models.Model):
+    OId = models.AutoField('OId', primary_key=True)
+    CodeName = models.CharField('CodeName', max_length=30)
+    Password = models.CharField('Password', max_length=20, null=False)
+    Permission = models.PositiveSmallIntegerField('Permission', null=False)
+    Mail = models.CharField('Mail', max_length=20)
+
+    class Meta:
+        db_table = 'user_account'
+
+    def __str__(self):
+        return self.CodeName
+
+
+class UserProfile(models.Model):
+    OId = models.AutoField('OId', primary_key=True)
+    CodeName = models.CharField('CodeName', max_length=30)
+    Gender = models.IntegerField('Gender', choices=((0, 'male'), (1, 'female')))  # 0表示男性，1表示女性
+    Class = models.CharField('Class', max_length=20)
+    Region = models.CharField('Region', max_length=30)
+    Race = models.CharField('Race', max_length=20)
+    Avatar = models.ImageField('Avatar', null=True)
+    Bio = models.TextField('Bio')
+
+    class Meta:
+        db_table = 'user_profile'
+
+    def __str__(self):
+        return self.CodeName
+
+
+class AccountApproveQueue(models.Model):
+    OId = models.AutoField('OId', primary_key=True)
+    CodeName = models.CharField('CodeName', max_length=30)
+    Password = models.CharField('Password', max_length=20, null=False)
+    Permission = models.IntegerField('Permission', null=False,default=3)
+    Class = models.CharField('Class', max_length=20)
+    Region = models.CharField('Region', max_length=30)
+    Race = models.CharField('Race', max_length=20)
+    Description = models.TextField('Description')
+
+    class Meta:
+        db_table = 'account_approve_queue'
+
+    def __str__(self):
+        return self.CodeName
--- backend/views.py
+++ backend/views.py
@@ -1,110 +1,68 @@
 from django.shortcuts import render
-from backend.tools import get_jwt, check_user, judge, all_users, modify_user, delete_user, consent, reject, \
-    one_user, add_into_queue, all_applications, modify_application, success, fail
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import CreateModelMixin, ListModelMixin, RetrieveModelMixin, UpdateModelMixin, \
+    DestroyModelMixin
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
-user_account = ['CodeName', 'Password', 'Permission', 'Class', 'Region', 'Race', 'Avatar', 'Mail']
-account_approve_queue = ['CodeName', 'Password', 'Permission', 'Class', 'Region', 'Race', 'Description']
-
-
-# 登录页面对应接口
-def login(request):
-    CodeName = request.POST.get("CodeName")
-    Password = request.POST.get("Password")
-    return check_user(CodeName, Password)
+from backend.tools import get_jwt, token2name
+from .serializers import UserAccountSerializer, AccountApproveQueue, \
+    AccountApproveQueueSerializer
+from .models import UserAccount
 
 
+class loginView(GenericAPIView, CreateModelMixin):
+    queryset = AccountApproveQueue.objects.all()
+    serializer_class = AccountApproveQueueSerializer
 
-def enroll(request):
-    CodeName = request.POST.get("CodeName")
-    Class = request.POST.get("Class")
-    Region = request.POST.get("Region")
-    Race = request.POST.get("Race")
-    Description = request.POST.get("Description")
-    Password = request.POST.get("Password")
-    return add_into_queue(CodeName, Class, Region, Race, Description, Password)
-
-
-# 用户表对应接口
-def users_get(request):
-    token = request.POST.get("token")
-    allowance = judge(token, 1)
-    if allowance:
-        return success('所有用户信息获取成功', all_users())
-    else:
-        return fail('无访问权限')
+    def post(self, request, module):
+        if module == 'login':
+            data = request.data
+            user = UserAccount.objects.get(CodeName=data['CodeName'])
+            if data['Password'] == user.Password:
+                token = get_jwt(user.CodeName)
+                result = {'CodeName': user.CodeName, 'token': token}
+                return Response(result)
+            else:
+                return Response('登录失败')
+        if module == 'enroll':
+            return self.create(request)
 
 
-def user_get(request):
-    token = request.POST.get("token")
-    return one_user(token)
+class UserListView(GenericAPIView, CreateModelMixin, ListModelMixin):
+    queryset = UserAccount.objects.all()
+    serializer_class = UserAccountSerializer
+
+    # permission_classes = [hasOpPermission, ]
+
+    def get(self, request):
+        return self.list(request)
+
+    def post(self, request):
+        return self.create(request)
 
 
-def user_modify(request):
-    token = request.POST.get("token")
-    code_name = request.POST.get("CodeName")
-    permission = request.POST.get("Permission")
-    allowance = judge(token, 1)
-    if allowance:
-        return modify_user(code_name, permission)
-    else:
-        return fail('无访问权限')
+class UserDetailView(GenericAPIView, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin):
+    queryset = UserAccount.objects.all()
+    serializer_class = UserAccountSerializer
+    lookup_field = 'CodeName'
+
+    def put(self, request, CodeName):
+        print(CodeName + '\n\n\n\n')
+        return self.update(request)
+
+    def get(self, request, CodeName):
+        return self.retrieve(request)
+
+    def delete(self, request, CodeName):
+        return self.destroy(request)
 
 
-def user_delete(request):
-    token = request.POST.get("token")
-    name = request.POST.get("CodeName")
-    allowance = judge(token, 1)
-    if allowance:
-        return delete_user(name)
-    else:
-        return fail('无访问权限')
-
-
-# 请求队列对应接口
-def application_reject(request):
-    token = request.POST.get("token")
-    name = request.POST.get("CodeName")
-    allowance = judge(token, 1)
-    if allowance:
-        return reject(name)
-    else:
-        return fail('无访问权限')
-
-
-def application_consent(request):
-    token = request.POST.get("token")
-    name = request.POST.get("CodeName")
-    allowance = judge(token, 1)
-    print("application_consent", name)
-    if allowance:
-        return consent(name)
-    else:
-        return fail('无访问权限')
-
-
-def applications_get(request):
-    token = request.POST.get("token")
-    allowance = judge(token, 1)
-    if allowance:
-        return success('所用待审批列表获取成功', all_applications())
-    else:
-        return fail('无访问权限')
-
-
-def application_modify(request):
-    token = request.POST.get("token")
-    CodeName = request.POST.get(account_approve_queue[0])
-    Password = request.POST.get(account_approve_queue[1])
-    Permission = request.POST.get(account_approve_queue[2])
-    Class = request.POST.get(account_approve_queue[3])
-    Region = request.POST.get(account_approve_queue[4])
-    Race = request.POST.get(account_approve_queue[5])
-    Description = request.POST.get(account_approve_queue[6])
-    allowance = judge(token, 1)
-    if allowance:
-        return modify_application(CodeName, Permission, Class, Region, Race, Description)
-    else:
-        return fail('无访问权限')
+class ApplicationModelView(ModelViewSet):
+    queryset = AccountApproveQueue.objects.all()
+    serializer_class = AccountApproveQueueSerializer
+    lookup_field = 'CodeName'
+    lookup_url_kwarg = 'CodeName'
 
 
 # 后端调试使用
