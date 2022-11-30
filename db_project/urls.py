e5ae4dfa89b99fec99170c6cd60a74c95221a9f8
rain1732 <3044550844@qq.com>
2022-12-01 01:26:39 +0800

    头像功能后端

--- db_project/urls.py
+++ db_project/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.static import serve
+from db_project.settings import MEDIA_ROOT, MEDIA_URL
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('backend.urls')),
-]
+                  path('admin/', admin.site.urls),
+                  path('', include('backend.urls')),
+                  path('upimg/<str:path>', serve, {"document_root": MEDIA_ROOT})
+              ] + static(MEDIA_URL, document_root=MEDIA_ROOT)
